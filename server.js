c055cfdeb82fbe7143ba37c5e7f0b8320a8af0da
Omegatupelo <revwitherspoon@yahoo.com>
2026-04-21 23:04:28 -0500

    first commit

---
+++ server.js
@@ -0,0 +1,39 @@
+const express = require("express");
+const path = require("path");
+
+const app = express();
+const PORT = 3000;
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+
+app.use(express.static(path.join(__dirname, "public")));
+
+app.get("/", (req, res) => {
+  res.render("index", {
+    pageTitle: "Prophecy: The Omega War Chronicles"
+  });
+});
+
+app.get("/about", (req, res) => {
+  res.render("about", {
+    pageTitle: "About the Author"
+  });
+});
+
+app.get("/characters/:name", (req, res) => {
+  const name = req.params.name.toLowerCase();
+
+  res.render(name, {
+    pageTitle: name.charAt(0).toUpperCase() + name.slice(1),
+  }, (err, html) => {
+    if (err) {
+      return res.status(404).send("Character not found");
+    }
+    res.send(html);
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}`);
+});
